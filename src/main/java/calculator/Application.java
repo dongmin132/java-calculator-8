f3e1e08182a95fa1809e6becf46792bbb9bd0ac2
dongmin132 <m914@naver.com>
2025-10-20 18:45:27 +0900

    feat(error): 잘못된 입력 시 오류 메시지 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,8 +9,11 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요. ");
         String input = Console.readLine();
 
-        int result = new StringCalculator().calculate(input);
-
-        System.out.println("결과 : " + result);
+        try {
+            int result = new StringCalculator().calculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error : " + e.getMessage());
+        }
     }
 }
