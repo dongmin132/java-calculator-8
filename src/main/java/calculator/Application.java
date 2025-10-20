295411afff512815b981e3efa79861aaa11c7125
dongmin132 <m914@naver.com>
2025-10-20 19:35:19 +0900

    fix(output): 출력 문구의 불필요한 공백 제거 및 불필요한 catch문 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,14 +6,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("덧셈할 문자열을 입력해 주세요. ");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        try {
-            int result = new StringCalculator().calculate(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error : " + e.getMessage());
-        }
+        int result = new StringCalculator().calculate(input);
+        System.out.println("결과 : " + result);
+
     }
 }
